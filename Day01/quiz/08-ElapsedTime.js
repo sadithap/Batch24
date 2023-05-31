a7829d8bfc767d91eac07ddd49239c7c5611c7e7
I Gede Ketut Saditha Putra <sadithaputra5@gmail.com>
2023-05-31 15:23:05 +0800

    koreksi

--- Day01/quiz/08-ElapsedTime.js
+++ Day01/quiz/08-ElapsedTime.js
@@ -9,11 +9,11 @@ function getPeriodTimes(seconds) {
         return (`${seconds} is not number`);
     }
     let toInt=parseInt(seconds);
-    var days = Math.floor(toInt / (24 * 60 * 60));
-    toInt %= 24 * 60 * 60;
+    var days = Math.floor(toInt / (24*60*60));
+    toInt %= 24*60*60;
 
-    var hours = Math.floor(toInt / (60 * 60));
-    toInt %= 60 * 60;
+    var hours = Math.floor(toInt / (60*60));
+    toInt %= 60*60;
 
     var minutes = Math.floor(toInt / 60);
     toInt %= 60;
